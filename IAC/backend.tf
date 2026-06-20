cfa9a08726b60428438c5e9d7f01c5fb7a79dae8
ch-saikumar-720 <chsaikumar720@gmail.com>
2026-06-20 20:31:10 +0530

    Revert "Terraform IAC code for the infra"
    This reverts commit bda796f3c3d9adbd436e9632f9e5bee172be7c2d.

--- IAC/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-tfstate-sai-kumar"
-    key          = "State/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}

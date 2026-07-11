a202f8b96c701866b43b8b0cee24cb205348c617
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2026-07-11 12:27:13 +0530

    Change S3 bucket name for Terraform backend

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-sai-kumar"
+    bucket       = "project-2026-statefile1"
     key          = "State/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
